23b7026e85bd1c47f4b637d6524234bde5802a02
computerYHJ <hyukjin5382@gmail.com>
2025-11-03 01:37:14 +0900

    feat: main() 함수 작성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,20 @@
 package lotto
 
+import lotto.output.BuyLottoPaper
+import lotto.output.CalculatorWinCount
+import lotto.output.PrintResult
+import lotto.output.UserLotto
+import lotto.output.UserPrice
+
 fun main() {
-    // TODO: 프로그램 구현
+    var price = UserPrice().getPrice()
+    var buyLotto = BuyLottoPaper(price)
+    var userLottoInfo = UserLotto()
+
+    var winCount: Array<Int> = CalculatorWinCount(
+        buyLotto.getLottoCount(), buyLotto.getLottoPaper(),
+        userLottoInfo.getWinner(), userLottoInfo.getBonus()
+    ).getRecordWinCount()
+
+    PrintResult(winCount, price)
 }

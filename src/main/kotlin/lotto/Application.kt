4e973a13bdd634669fe55aca9143946351b1ab81
computerYHJ <hyukjin5382@gmail.com>
2025-11-03 18:29:03 +0900

    예외 감지 시 다시 실행 기능

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,17 +3,18 @@ package lotto
 import lotto.output.BuyLottoPaper
 import lotto.output.CalculatorWinCount
 import lotto.output.PrintResult
+import lotto.output.UserInfo
 import lotto.output.UserLotto
 import lotto.output.UserPrice
 
 fun main() {
-    var price = UserPrice().getPrice()
+    var price = UserInfo.getPrice()
     var buyLotto = BuyLottoPaper(price)
-    var userLottoInfo = UserLotto()
+    //var userLottoInfo = UserLotto()
 
     var winCount: Array<Int> = CalculatorWinCount(
         buyLotto.getLottoCount(), buyLotto.getLottoPaper(),
-        userLottoInfo.getWinner(), userLottoInfo.getBonus()
+        UserInfo.getWinNum(), UserInfo.getBonus()
     ).getRecordWinCount()
 
     PrintResult(winCount, price)
---
+++ src/main/kotlin/lotto/output/UserInfo.kt
@@ -0,0 +1,51 @@
+package lotto.output
+
+import lotto.input.UserInput
+import lotto.validator.InputValidator
+
+object UserInfo {
+
+    private var price: Int = -1
+    private var winnerNumber: List<Int> = emptyList()
+    private var bonusNumber: Int = -1
+
+    fun getPrice(): Int{
+        while(price == -1) correctPrice()
+        return price
+    }
+
+    fun correctPrice() {
+        try {
+            price = InputValidator.validatorPrice(UserInput.userInputPrice())
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+
+    fun getWinNum(): List<Int>{
+        while(winnerNumber.isEmpty()) correctWinNum()
+        return winnerNumber
+    }
+
+    fun correctWinNum(){
+        try{
+            winnerNumber = InputValidator.validatorLotto(UserInput.userInputLotto())
+        } catch (e: IllegalArgumentException){
+            println(e.message)
+        }
+    }
+
+    fun getBonus(): Int{
+        while(bonusNumber == -1) correctBonus()
+        return bonusNumber
+    }
+
+    fun correctBonus() {
+        try {
+            bonusNumber = InputValidator.validatorBonus(UserInput.userInputBonus(), winnerNumber)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+
+}
